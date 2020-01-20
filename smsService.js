9a4b0f98f8d786cb29632e797881f9633beea2ee
harsh-a1 <harsh.atal@gmail.com>
2020-01-20 11:30:12 +0530

    minor fix

--- smsService.js
+++ smsService.js
@@ -12,38 +12,12 @@ var request = require('request');
 
 function smsService(){
     
-
     this.sendSMS = function(to,message,callback){           
-
+      
         __logger.info("Sending Verification Message");
         
-        // Use the service
-        const options = {
-            "username" : config.sms.username,
-            "to": to,
-            "message": message,
-            "username" : config.sms.username
-
-        }
-
-  /*      request({
-            url:  config.sms.base_url,
-            method: "POST",
-            //    json: true,   // <--Very important!!!
-            body: JSON.stringify(options),
-            headers: {
-                "apiKey": config.sms.apikey,
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Accept" : "application/json"
-
-            }
-        }, function (error, response, body) {
-            debugger
-            //callback(error,response,body);
-        });
-
-*/
-
+        to = to.split(",");
+        
         // Initialize the SDK
         const AfricasTalking = require('africastalking')({
             apiKey: config.sms.apikey,
@@ -55,9 +29,10 @@ function smsService(){
         const sms = AfricasTalking.SMS;
 
         function sendMessage() {
+          
             const options = {
                 // Set the numbers you want to send to in international format
-                to: [to],
+                to: to,
                 // Set your message
                 message: message,
                 // Set your shortCode or senderId
@@ -82,3 +57,21 @@ function smsService(){
         
     }
 }
+
+  /*      request({
+            url:  config.sms.base_url,
+            method: "POST",
+            //    json: true,   // <--Very important!!!
+            body: JSON.stringify(options),
+            headers: {
+                "apiKey": config.sms.apikey,
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Accept" : "application/json"
+
+            }
+        }, function (error, response, body) {
+            debugger
+            //callback(error,response,body);
+        });
+
+*/
